a132026fd31966a81b40aa4a84065fbe96d09f57
Yiorgis Gozadinos <ggozad@jarn.com>
2011-08-30 13:59:44 +0200

    vb

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ Operating System :: Unix
 """
 
 setup(name='Products.ZPsycopgDA',
-      version='2.0.13-jarn1',
+      version='2.0.13-jarn2',
       description='Python-PostgreSQL Zope Database Adapter.',
       classifiers=filter(None, classifiers.split("\n")),
       maintainer="Federico Di Gregorio",
